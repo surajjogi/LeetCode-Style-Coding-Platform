37b04f5fe1e36bb8bfec453dbc02eb6f02dc3f48
suraj jogi <surajjogi078@gmail.com>
2026-04-22 01:20:33 +0530

    update axios client settings

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'https://leetcode-style-coding-platform-2.onrender.com',
+const axiosClient = axios.create({
+    baseURL: 'https://leetcode-style-coding-platform-1.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
